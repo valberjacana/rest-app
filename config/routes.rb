720782caa7056d35273f9d392481721a930693b3
valberjacana <vbermudeza@yahoo.com>
2019-02-02 11:54:27 -0300

    chore: user's scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  #Crea o enlaza el recurso de user, con esto lo enruta a la BD
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#permite eliminar todo el contenido de la tabla
+User.delete_all
+
+#se van a crear 5 usuaruis
+5.times do |index|
+    #el simbolo de ! permite que nos indique el error
+    User.create!(name: "nombre#{index}", age: 33)
+
+end 
